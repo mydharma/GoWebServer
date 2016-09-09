f9f52593ab184068c9e4512f17ef06fcc0aba77e
mydharma <gopichaithanya@hotmail.com>
2016-09-08 21:08:26 -0700

    Some change

--- Server.go
+++ Server.go
@@ -6,7 +6,19 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Yes, it works")
+	//io.WriteString(w, "Yes, it works")
+	io.WriteString(w,
+			`<!DOCTYPE html>
+			<html>
+			<body>
+
+			<h1>
+					   It works!!!
+			</h1>
+
+			</body>
+			</html>
+			`)
 }
 
 func main() {
